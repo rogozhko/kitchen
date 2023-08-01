acefa7c15425f157de6ec652578ed03f99e320c8
Nikolay Rogozhko <rogozhko@gmail.com>
2023-08-02 01:51:16 +0300

    add input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
-
+    [SerializeField] private GameInput gameInput;
+    
+    
     private bool isWalking;
     
     private void Update()
     {
-        Vector2 inputVector = new Vector2();
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1f;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * (moveSpeed * Time.deltaTime);
@@ -36,7 +19,8 @@ public class Player : MonoBehaviour
         isWalking = moveDir != Vector3.zero;
         
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        transform.forward = Vector3.Slerp(
+            transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
     public bool IsWalking()
